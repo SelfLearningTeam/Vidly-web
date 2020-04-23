15b7c9f6bb20ee6987d9f72578c12f7cc8484ff1
purevdorj <puwe.baatar@gmail.com>
2020-04-23 12:04:19 +0900

    data

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 using Vidly.Models;
 using Vidly.ViewModels;
 
@@ -10,16 +11,29 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+        
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
                 return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
---
+++ Migrations/202004230142000_SetNamesOfMembershipType.cs
@@ -0,0 +1,20 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SetNamesOfMembershipType : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes SET NAME = 'Pay as You go' WHERE Id = 1");
+            Sql("UPDATE MembershipTypes SET NAME = 'Monthly' WHERE Id = 2");
+            Sql("UPDATE MembershipTypes SET NAME = 'Quarterly' WHERE Id = 3");
+            Sql("UPDATE MembershipTypes SET NAME = 'Annual' WHERE Id = 4");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
